d5936b2abcb01a9ce703a0c84a20e8f24c4564ef
Earl Chandler <earllchandler@gmail.com>
2022-04-01 13:22:03 -0400

    cleaned out pages not needed for project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"; //use state hook used to create and update variables
 
 function App() {
+  // const [entries, setEntries] = useState([{weight: }])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>Hello World</h3>
     </div>
   );
 }
